a434969bde99d271598a479b59a017b082af0285
Hayden Young <hayden@kuraudo.io>
2024-01-22 23:32:58 +0000

    feat: init main addon

---
+++ addons/main/config.cpp
@@ -0,0 +1,14 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"cba_main"};
+        author = "PMC Sentinel Team";
+        url = CSTRING(url);
+        VERSION_CONFIG;
+    };
+};
