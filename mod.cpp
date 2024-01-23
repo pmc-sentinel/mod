2a352dc586df69b0f95be2b6cc517502de28809d
Hayden Young <hayden@kuraudo.io>
2024-01-23 01:18:41 +0000

    feat(ci): github actions workflow

--- mod.cpp
+++ mod.cpp
@@ -1,14 +1,14 @@
-name = "PMC Sentinel";
-picture = "logo_pmcs.paa";
-actionName = "Discord";
-action = "https://discord.gg/Ctfu2DTb7f";
-description = "";
-logo = "logo_pmcs.paa";
-logoOver = "logo_pmcs.paa";
-tooltip = "PMC Sentinel";
-tooltipOwned = "PMC Sentinel Owned";
-overview = "pepega amirite";
-author = "PMC Sentinel Team";
-overviewPicture = "logo_pmcs.paa";
-overviewText = "PMC Sentinel overviewText";
-overviewFootnote = "yep";
+name             = "PMC Sentinel";
+author           = "PMC Sentinel Team";
+
+actionName       = "Discord";
+action           = "https://discord.gg/Ctfu2DTb7f";
+
+overview         = "Adds PMC Sentinel as a playable faction.";
+
+picture          = "logo_pmcs.paa";
+logo             = "logo_pmcs.paa";
+logoOver         = "logo_pmcs.paa";
+
+hideName         = 0;
+hidePicture      = 0;
