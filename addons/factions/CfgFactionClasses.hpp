96153b71621fcc92cbdfa67f4469b8342055fa3e
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-08-30 17:27:10 +0100

    Rename faction "PMC Sentinel (D)"

--- addons/factions/CfgFactionClasses.hpp
+++ addons/factions/CfgFactionClasses.hpp
@@ -1,6 +1,6 @@
 class CfgFactionClasses {
 	class PMCS_B {
-		displayName = CSTRING(BLUFOR_DisplayName);
+		displayName = CSTRING(BLUFOR_DisplayName_D);
 		icon = "z\pmcs\addons\main\logo_pmcs.paa";
 		priority = 1;
 		side = 1; // BLUFOR
