f35a10859da3092ca73a63b486f3af7a84e24dfd
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-09-13 19:08:26 +0100

    Map markers based on teams

---
+++ addons/markers/CfgMarkers.hpp
@@ -0,0 +1,9 @@
+#define MARKER(NAME,IMG) class GVAR(NAME) { name = QUOTE(NAME); icon = QUOTE(z\pmcs\addons\markers\##IMG##.paa); color[] = {1,1,1,1}; size = 32; shadow = 0; scope = 2; markerClass = "Flags"; }
+
+class CfgMarkers {
+	MARKER(Archer,archer);
+	MARKER(Bandit,bandit);
+	MARKER(Castle,castle);
+	MARKER(Warden,warden);
+	MARKER(Command,command);
+};
---
+++ addons/markers/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT markers
+#define COMPONENT_BEAUTIFIED Markers
+#include "\z\pmcs\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_MARKERS
+	#define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_MARKERS
+	#define DEBUG_SETTINGS DEBUG_SETTINGS_MARKERS
+#endif
+
+#include "\z\pmcs\addons\main\script_macros.hpp"
