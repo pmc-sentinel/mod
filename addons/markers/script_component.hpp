4bd4ccb9d9c91b404a58443009610333ad04bf2f
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-09-13 19:55:36 +0100

    Tabs to spaces

--- addons/markers/script_component.hpp
+++ addons/markers/script_component.hpp
@@ -7,11 +7,11 @@
 // #define ENABLE_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_MARKERS
-	#define DEBUG_MODE_FULL
+    #define DEBUG_MODE_FULL
 #endif
 
 #ifdef DEBUG_SETTINGS_MARKERS
-	#define DEBUG_SETTINGS DEBUG_SETTINGS_MARKERS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_MARKERS
 #endif
 
 #include "\z\pmcs\addons\main\script_macros.hpp"
