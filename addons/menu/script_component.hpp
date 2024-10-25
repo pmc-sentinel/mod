2633e75e50288d4da16a90e1ef0e41e397103ab7
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-10-25 17:09:41 +0100

    feat: custom spotlight buttons for pmc sentinel servers

---
+++ addons/menu/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT menu
+#define COMPONENT_BEAUTIFIED Menu
+#include "\z\pmcs\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_MENU
+	#define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_MENU
+	#define DEBUG_SETTINGS DEBUG_SETTINGS_MENU
+#endif
+
+#include "\z\pmcs\addons\main\script_macros.hpp"
